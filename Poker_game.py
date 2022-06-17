066d86802174664553111a48ce70229e6b092ddd
Cryingpepe <82265937+Cryingpepe@users.noreply.github.com>
2022-06-17 21:57:59 +0900

    Update Poker_game.py

--- Poker_game.py
+++ Poker_game.py
@@ -1,12 +1,10 @@
 class Cards:
     shapes = ['\U00002660', '\U00002665', '\U00002663', '\U00002666']
     ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-    deck = []
 
     def __init__(self, shape, rank):
         self.shape = shape
         self.rank = rank
-        self.deck = deck
     
     def shape(self):
         return self.shape
@@ -14,10 +12,16 @@ class Cards:
     def rank(self):
         return self.rank
     
-    def deck
-    
     def __str__(self):
         return self.shape, self.ranks
 
 class Hand:
-    if
+    hand = []
+
+    def draw(self):
+
+Deck = []
+
+for i in range(4):
+    for x in range(13):
+        Deck.append([Cards.shapes[i],Cards.ranks[x]])
